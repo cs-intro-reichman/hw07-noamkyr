7058ae071948ff89c1fc26fd20a39119171cfdc7
noam <noam.kyram@post.runi.ac.il>
2024-01-29 21:21:22 +0200

    documentation

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -5,30 +5,34 @@ public class HashTagTokenizer {
 	public static void main(String[] args) {
 
 		String hashTag = args[0];
-		//String hashTag = "iloverecursion";
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
+		// set new array
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
+		// add each word to new index at the array
 		for (int i = 0; i < dictionary.length; i++) {
 			String current  = in.readString();
 			dictionary[i] = current;
 		}
-
+		// return the array
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
+
+		// check each word in the dictionary and check if equals to the word
 		for (int i = 0; i < dictionary.length; i++) {
 			if (word.equals(dictionary[i])){
+				// word found
 				return true;
 			}
 		}
+		// word not found
 		return false;
 	}
 
@@ -40,16 +44,23 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
-		boolean has_found = false;
-		int next_index = 0;
+		boolean has_found = false; // holds if the current word has found
+		int next_index = 0; // holds the index of the next word in the string
+
+		// check from the first letter if real word and extend if not
         for (int i = 1; i <= N && has_found == false; i++) {
+			// set the word to lowercase
 			String current = hashtag.substring(0,i).toLowerCase();
+
+			// if the word in the dictionary
 			if(existInDictionary(current, dictionary)){
 				has_found = true;
 				next_index = i;
 				System.out.println(current);
 			}
         }
+
+		// if the word was found do the same check from the start of the next word
 		if (has_found){
 			breakHashTag(hashtag.substring(next_index), dictionary);
 		} else {
--- SpellChecker.java
+++ SpellChecker.java
@@ -4,11 +4,6 @@ public class SpellChecker {
 
 	public static void main(String[] args) {
 		String word = args[0];
-		//String word = "wombat";
-		//String word1 = "hello1";
-		//String word2 = "hpll03";
-		//System.out.println(levenshtein(word1, word2));
-
 
 		int threshold = Integer.parseInt(args[1]);
 		//int threshold = 2;
@@ -16,14 +11,20 @@ public class SpellChecker {
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
 	}
-
+	// return the string without the first char
 	public static String tail(String str) {
 		return str.substring(1).toLowerCase();
 	}
 
+
+	// return the distance between the words according the formula
 	public static int levenshtein(String word1, String word2) {
+
+		// set the words to lowercase
 		word1 = word1.toLowerCase();
 		word2 = word2.toLowerCase();
+
+		// return the difference of the length
 		if (word1.isEmpty()){
 			if (word2.isEmpty()){
 				return 0;
@@ -40,11 +41,13 @@ public class SpellChecker {
 			}
 
 		}
-		int count = 0;
 
+		// check the first letters
 		if (word1.charAt(0) == word2.charAt(0)){
+			// return the tails if equals
 			return levenshtein(tail(word1), tail(word2));
 		} else {
+			// return the min between the recursive calls and add 1
 			int min =  Math.min(levenshtein(tail(word1), word2), levenshtein(tail(word1), tail(word2)));
 			min = Math.min(min, levenshtein(word1, tail(word2)));
 			return 1 + min;
@@ -52,38 +55,46 @@ public class SpellChecker {
 	}
 
 	public static String[] readDictionary(String fileName) {
+		// set new array
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
-
+		// add each word to each index at the array
 		for (int i = 0; i < dictionary.length; i++) {
 			String current  = in.readString();
 			dictionary[i] = current;
 		}
-
+		// return the dictionary
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 
+		// holds the minimal distance
 		int min = -1;
+
+		// holds the closest word
 		String closest = "";
+
+		// check each word in the dictionary
 		for (int i = 0; i < dictionary.length; i++) {
+
+			// get the distance of the current word
 			int distance = levenshtein(word,dictionary[i]);
+			// if the words are equals
 			if (distance == 0){
 				return dictionary[i];
 			}
 
-			if (dictionary[i].equals("woman")){
-				int x = 3;
-			}
-
+			// if the current word below the standard
 			if (distance <= threshold){
 
+				// if the first word below threshold update the minimal distance and the minimal distance
 				if (min == -1){
 					closest = dictionary[i];
 					min = distance;
 				} else {
+					// update the minimal distance and the minimal distance
 					if (distance < min){
 						min = distance;
 						closest = dictionary[i];
@@ -92,11 +103,12 @@ public class SpellChecker {
 			}
 
 		}
-
+		// if no world with lower threshold return the original word
 		if (min == -1){
 			return word;
 		}
 
+		// return the closest
 		return closest;
 	}
 
