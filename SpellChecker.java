b17475f42ac68a76a152a7db33b44cbf97fdb7cc
noam <noam.kyram@post.runi.ac.il>
2024-01-30 20:47:44 +0200

    woman check

--- SpellChecker.java
+++ SpellChecker.java
@@ -5,6 +5,7 @@ public class SpellChecker {
 	public static void main(String[] args) {
 		String word = args[0];
 
+
 		int threshold = Integer.parseInt(args[1]);
 		//int threshold = 2;
 		String[] dictionary = readDictionary("dictionary.txt");
